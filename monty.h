963136a90f4665c25c3da740b346f29ffa153ab1
yassine goumni <40956494+Blackarrow299@users.noreply.github.com>
2023-06-23 13:55:50 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -36,43 +36,8 @@ char* readIden(char* input ,size_t* pos);
 char* readInt(char* input ,size_t* pos);
 Token idenToToken(char* iden,unsigned int line_number);
 TokenNode* getTokensByLine(TokenNode* head,unsigned int line_number);
+
 extern TokenNode* TokenNodeHead;
 
-/**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
- */
-typedef struct stack_s
-{
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
-} stack_t;
-
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
-typedef struct instruction_s
-{
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
-
-instruction_t* getInstructions();
-instruction_t* getInstructionByOpcode(instruction_t* instructions, char* opcode);
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-void freeTokens(TokenNode *head);
 void freeStack(stack_t **head);
 #endif
